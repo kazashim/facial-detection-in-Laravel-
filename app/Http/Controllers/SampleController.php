24668778fcfa42211ea5e6c549c776ef3ea2cf61
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-23 18:39:59 +0200

    Update SampleController.php

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Google\Cloud\Core\ServiceBuilder;
 
 class SampleController extends Controller
 {
-    //
+    public function detectFaces()    {        //Code will be added here  
+     }
 }
